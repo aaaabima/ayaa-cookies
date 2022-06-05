afddc0add16dc49c55c945c38a57e390d0a79018
aaaabima <aaaabim@gmail.com>
2022-06-05 19:09:45 +0700

    Front end done

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='w-full h-16 bg-slate-400 text-center'>
+      <p className='py-4 h-8 text-sm'>Mochamad Arya Bima Agfian - 140810190031</p>
+    </div>
+  )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import { axios } from "axios";
-import qs from 'qs';
+import axios from "axios";
+import qs from "qs";
 import { useState, useEffect } from "react";
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Footer from "../components/footer";
 
 export default function Home() {
   const [value, setValue] = useState({
@@ -68,66 +67,66 @@ export default function Home() {
     }
   };
 
-  // const getData = async () => {
-  //   const BASE_URL = "http://localhost:3030/cookies/query";
+  const getData = async () => {
+    const BASE_URL = "http://localhost:3030/cookies/query";
 
-  //   const headers = {
-  //     Accept: "application/sparql-results+json,*/*;q=0.9",
-  //     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-  //   };
+    const headers = {
+      Accept: "application/sparql-results+json,*/*;q=0.9",
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    };
 
-  //   const queryData = {
-  //     query: `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-  //     PREFIX d: <http://ayaa-cookies.com/data#>
-  //     SELECT ?tipe ?nama ?rasa ?tepung ?telur ?gula ?mentega ?keju ?garam ?deskripsi
-  //     WHERE {
-  //       ?sub d:tipe ?tipe.
-  //       ?sub d:nama ?nama.
-  //       ?sub d:rasa ?rasa.
-  // 		  ?sub d:tepung ?tepung.
-  //       ?sub d:telur ?telur.
-  //       ?sub d:gula ?gula.
-  //       ?sub d:mentega ?mentega.
-  //       ?sub d:keju ?keju.
-  //       ?sub d:garam ?garam.
-  //       ?sub d:deskripsi ?deskripsi.
-  //     FILTER(
-  //       regex(?tipe, "^${value.input}", "i") ||
-  //       regex(?nama, "^${value.input}", "i") ||
-  //       regex(?rasa, "^${value.input}", "i") ||
-  //       regex(?tepung, "^${value.input}", "i") ||
-  //       regex(?telur, "^${value.input}", "i") ||
-  //       regex(?gula, "^${value.input}", "i") ||
-  //       regex(?mentega, "^${value.input}", "i") ||
-  //       regex(?keju, "^${value.input}", "i") ||
-  //       regex(?garam, "^${value.input}", "i") ||
-  //       regex(?deskripsi, "^${value.input}", "i")
-  //     )
-  //     } `,
-  //   };
+    const queryData = {
+      query: `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX d: <http://ayaa-cookies.com/data#>
+      SELECT ?tipe ?nama ?rasa ?tepung ?telur ?gula ?mentega ?keju ?garam ?deskripsi
+      WHERE {
+        ?sub d:tipe ?tipe.
+        ?sub d:nama ?nama.
+        ?sub d:rasa ?rasa.
+  		  ?sub d:tepung ?tepung.
+        ?sub d:telur ?telur.
+        ?sub d:gula ?gula.
+        ?sub d:mentega ?mentega.
+        ?sub d:keju ?keju.
+        ?sub d:garam ?garam.
+        ?sub d:deskripsi ?deskripsi.
+      FILTER(
+        regex(?tipe, "${value.input}", "i") ||
+        regex(?nama, "${value.input}", "i") ||
+        regex(?rasa, "${value.input}", "i") ||
+        regex(?tepung, "${value.input}", "i") ||
+        regex(?telur, "${value.input}", "i") ||
+        regex(?gula, "${value.input}", "i") ||
+        regex(?mentega, "${value.input}", "i") ||
+        regex(?keju, "${value.input}", "i") ||
+        regex(?garam, "${value.input}", "i") ||
+        regex(?deskripsi, "${value.input}", "i")
+      )
+      } `,
+    };
 
-  //   try {
-  //     const { data } = await axios(BASE_URL, {
-  //       method: "POST",
-  //       headers,
-  //       data: qs.stringify(queryData),
-  //     });
-  //     console.log(data);
+    try {
+      const { data } = await axios(BASE_URL, {
+        method: "POST",
+        headers,
+        data: qs.stringify(queryData),
+      });
+      console.log(data);
 
-  //     // Convert Data
-  //     const formatted_data = data.results.bindings.map((code, index) =>
-  //       formatter(code, index)
-  //     );
-  //     console.log(formatted_data);
+      // Convert Data
+      const formatted_data = data.results.bindings.map((code, index) =>
+        formatter(code, index)
+      );
+      console.log(formatted_data);
 
-  //     setValue({
-  //       ...value,
-  //       codes: formatted_data,
-  //     });
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
+      setValue({
+        ...value,
+        codes: formatted_data,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   const formatter = (codes, index) => {
     return {
@@ -153,7 +152,7 @@ export default function Home() {
   };
 
   const content = value.codes.map((code) => (
-    <tr key={code.id}>
+    <tr key={code.id} id={code.id % 2 === 1 ? "odd" : "even"}>
       <td>{code.id + 1}</td>
       <td>{code.nama}</td>
       <td>{code.tipe}</td>
@@ -164,31 +163,36 @@ export default function Home() {
       <td>{code.mentega}</td>
       <td>{code.keju}</td>
       <td>{code.garam}</td>
-      <td>{code.deskripsi}</td>
+      <td className="deskripsi">{code.deskripsi}</td>
     </tr>
   ));
 
-  // useEffect(() => {
-  //   getAllData();
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
+  useEffect(() => {
+    getAllData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <div>
-      {/* <form className="search-bar" onSubmit={(e) => e.preventDefault()}>
-        <div>
-          <div>
-            <input
-              type="text"
-              placeholder="Search..."
-              setvalue={value.input}
-              onChange={handleChange}
-              required="required"
-              onKeyPress={(event) => {
-                event.key === "Enter" && getData();
-              }}
-            />
+    // <p>Hello</p>
+    <>
+      <div className="font-asap mt-4 mx-4">
+        <div className="w-full mx-auto text-center bg-slate-400 rounded">
+          <div className="py-4">
+            <p className="text-6xl font-bold">ayaa-cookies</p>
+            <p className="mt-4 text-lg">Temukan kue-kue sederhana yang Ayaa Cookies buat di sini.</p>
+          </div>
+          <form className="search-bar" onSubmit={(e) => e.preventDefault()}>
             <div>
+              <input
+                type="text"
+                placeholder="Search..."
+                setvalue={value.input}
+                onChange={handleChange}
+                required="required"
+                onKeyPress={(event) => {
+                  event.key === "Enter" && getData();
+                }}
+              />
               <button
                 className="search-button"
                 type="button"
@@ -198,25 +202,28 @@ export default function Home() {
                 <span>Search</span>
               </button>
             </div>
-          </div>
+          </form>
         </div>
-      </form> */}
-      <table>
-        <tr>
-          <th>No</th>
-          <th>Nama</th>
-          <th>Tipe Kue</th>
-          <th>Rasa</th>
-          <th>Tepung</th>
-          <th>Telur</th>
-          <th>Gula</th>
-          <th>Mentega</th>
-          <th>Keju</th>
-          <th>Garam</th>
-          <th>Deskripsi</th>
-        </tr>
-        {content}
-      </table>
-    </div>
+        <div>
+          <table className="cookies-list">
+            <tr>
+              <th>No</th>
+              <th>Nama</th>
+              <th>Tipe Kue</th>
+              <th>Rasa</th>
+              <th>Tepung</th>
+              <th>Telur</th>
+              <th>Gula</th>
+              <th>Mentega</th>
+              <th>Keju</th>
+              <th>Garam</th>
+              <th>Deskripsi</th>
+            </tr>
+            {content}
+          </table>
+        </div>
+      </div>
+      <Footer className="absolute bottom-0"/>
+    </>
   );
 }
